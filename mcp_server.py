e742f1cd9d68321aef06f330678d3a5337c84c56
Chetanoo <chetanvse@gmail.com>
2026-04-28 01:48:29 +0400

    defining resources

--- mcp_server.py
+++ mcp_server.py
@@ -34,8 +34,16 @@ def edit_document(
     if doc_id not in docs:
         raise ValueError(f"Doc with {doc_id} not found.")
     docs[doc_id] = docs[doc_id].replace(old_contents, new_contents)
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+
+@mcp.resource("docs://documents/", mime_type="application/json")
+def list_documents() -> list[str]:
+    return list(docs.keys())
+
+@mcp.resource("docs://documents/{doc_id}", mime_type="text/plain")
+def get_document(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found.")
+    return docs[doc_id]
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
